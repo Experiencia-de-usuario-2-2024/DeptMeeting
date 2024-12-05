a5dd83d388163713cdbed7d9a455e83e2ea0dfce
onicalls <nicolas.rojas.g@usach.cl>
2024-12-05 11:30:04 -0400

    (deploy) docker changes

--- frontend/docker-push-all.bat
+++ frontend/docker-push-all.bat
@@ -1,17 +1,17 @@
-docker push pipetboy/text-edit-front-principal:v3
+docker push jyr20/text-edit-front-principal:v3
 echo "front-principal pusheado!"
-docker push pipetboy/text-edit-mf-home:v3
+docker push jyr20/text-edit-mf-home:v3
 echo "mf-home pusheado!"
-docker push pipetboy/text-edit-mf-informacion:v3
+docker push jyr20/text-edit-mf-informacion:v3
 echo "mf-informacion pusheado!"
-docker push pipetboy/text-edit-mf-kanbanplus:v3
+docker push jyr20/text-edit-mf-kanbanplus:v3
 echo "mf-kanbanplus pusheado!"
-docker push pipetboy/text-edit-mf-login:v3
+docker push jyr20/text-edit-mf-login:v3
 echo "mf-login pusheado!"
-docker push pipetboy/text-edit-mf-perfil:v3
+docker push jyr20/text-edit-mf-perfil:v3
 echo "mf-perfil pusheado!"
-docker push pipetboy/text-edit-mf-proyectos:v3
+docker push jyr20/text-edit-mf-proyectos:v3
 echo "mf-proyectos pusheado!"
-docker push pipetboy/text-edit-mf-tareas:v3
+docker push jyr20/text-edit-mf-tareas:v3
 echo "mf-tareas pusheado!"
 
